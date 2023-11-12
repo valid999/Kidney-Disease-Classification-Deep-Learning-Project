af7465012544178aec6f8d2af65d917e349366e7
Waleed <validjd48@gmail.com>
2023-11-12 18:00:37 +0500

    requested

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "Kidney-Disease-Classification-Deep-Learning-Project"
-AUTHOR_USER_NAME = "krishnaik06"
+REPO_NAME = "MLflow-Dagshub-open-source"
+AUTHOR_USER_NAME = "valid999"
 SRC_REPO = "cnnClassifier"
-AUTHOR_EMAIL = "entbappy73@gmail.com"
+AUTHOR_EMAIL = "valid999@gmail.com"
 
 
 setuptools.setup(
